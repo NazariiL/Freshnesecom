b4e7ae3156ea207fe77ed85d8c879cc8905f3fae
LagoidaN <nazarsnik@gmail.com>
2023-07-15 00:02:02 +0300

    header and components

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,15 @@
 const { src, dest, watch, parallel } = require('gulp'),
-  scss = require('gulp-sass')(require('sass')),
-  concat = require('gulp-concat'),
-  uglify = require('gulp-uglify-es').default,
   browserSync = require('browser-sync').create(),
+  scss = require('gulp-sass')(require('sass')),
+  uglify = require('gulp-uglify-es').default,
+  plumber = require('gulp-plumber'),
   pug = require('gulp-pug'),
   // Pathes
   SCRIPT_URL = './dist/scripts/*.js',
   CSS_URL = './scss/index.scss',
-  PUG_URL = './src/*.pug',
-  plumber = require('gulp-plumber');
+  PUG_URL = './src/*.pug';
 
-const scripts = () =>
-  src(SCRIPT_URL)
-    .pipe(plumber())
-    .pipe(uglify())
-    .pipe(dest('dist/scripts'))
-    .pipe(browserSync.stream());
+const scripts = () => src(SCRIPT_URL).pipe(plumber()).pipe(uglify()).pipe(dest('dist/scripts')).pipe(browserSync.stream());
 
 const styles = (prodMode = false) =>
   src(CSS_URL)
@@ -24,8 +18,7 @@ const styles = (prodMode = false) =>
     .pipe(dest('dist/css'))
     .pipe(browserSync.stream());
 
-const pugTask = (prodMode = false) =>
-  src(PUG_URL).pipe(pug()).pipe(dest('dist/')).pipe(browserSync.stream());
+const pugTask = (prodMode = false) => src(PUG_URL).pipe(pug()).pipe(dest('dist/')).pipe(browserSync.stream());
 
 const watcher = () => {
   watch(['./scss/**/*.scss'], styles);
